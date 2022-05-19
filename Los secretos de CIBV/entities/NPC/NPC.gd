646afef61b465b41a2c01bb46471a10c08c1ae69
HectorVillavicencio <103118349+HectorVillavicencio@users.noreply.github.com>
2022-05-19 12:54:39 -0300

    Agregue una interrogacion al NPC

--- Los secretos de CIBV/entities/NPC/NPC.gd
+++ Los secretos de CIBV/entities/NPC/NPC.gd
@@ -1,17 +1,26 @@
 extends KinematicBody2D
 
+onready var question = $Question
 
+var active = false
 
 func _ready():
 	pass # Replace with function body.
 
 
+func _process(delta):
+	question.visible = active
 
 func _on_AreaEvent_body_entered(body):
 	if body.has_method("taked"):
+		active = true
 		if body.papel !=3:
 			print("No tienes todos los papeles")
 		else:
 			print("Te dejo pasar, pero es bajo tu propio riesgo...")
 			Controller.abrir_puerta()
 			body.papel = 0
+
+
+func _on_AreaEvent_body_exited(body):
+	active = false
