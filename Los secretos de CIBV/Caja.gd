6e721168ed236a156528ac6ef99f6ec48759cf40
HectorVillavicencio <103118349+HectorVillavicencio@users.noreply.github.com>
2022-06-20 13:25:25 -0300

    arregle la caja

--- Los secretos de CIBV/Caja.gd
+++ Los secretos de CIBV/Caja.gd
@@ -2,6 +2,11 @@ extends StaticBody2D
 
 onready var tween:Tween = $Tween
 onready var raycast:RayCast2D = $RayCast2D
+onready var raycast1:RayCast2D = $RayCast2D2
+onready var raycast2:RayCast2D = $RayCast2D3
+onready var raycast3:RayCast2D = $RayCast2D4
+onready var raycast4:RayCast2D = $RayCast2D5
+onready var raycast5:RayCast2D = $RayCast2D6
 
 export (Vector2) var grid_cell_size: Vector2
 
@@ -16,10 +21,20 @@ func _on_Area2D_body_entered(body: Node) -> void:
 		print(final_direction)
 		
 		raycast.cast_to = final_direction * grid_cell_size
+		raycast1.cast_to = final_direction * grid_cell_size
+		raycast2.cast_to = final_direction * grid_cell_size
+		raycast3.cast_to = final_direction * grid_cell_size
+		raycast4.cast_to = final_direction * grid_cell_size
+		raycast5.cast_to = final_direction * grid_cell_size
 		raycast.force_raycast_update()
-		if !raycast.is_colliding():
-			var final_pos: Vector2 = position + final_direction * grid_cell_size
-			tween.interpolate_property(self, "position", position, final_pos, 1.5, Tween.TRANS_SINE)
+		raycast1.force_raycast_update()
+		raycast2.force_raycast_update()
+		raycast3.force_raycast_update()
+		raycast4.force_raycast_update()
+		raycast5.force_raycast_update()
+		if !raycast.is_colliding() && !raycast1.is_colliding() && !raycast2.is_colliding() && !raycast3.is_colliding() && !raycast4.is_colliding() && !raycast5.is_colliding():
+			var final_pos: Vector2 = position + final_direction * grid_cell_size 
+			tween.interpolate_property(self, "position", position, final_pos, 1, Tween.TRANS_SINE)
 			tween.start()
 		else:
 			print("No me puedo mover!")
